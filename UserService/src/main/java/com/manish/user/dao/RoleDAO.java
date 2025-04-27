53fccd1254710558e0faf09ba5ceb4fdc033bcc8
manish <46858945+manish465@users.noreply.github.com>
2025-04-27 11:27:48 +0530

    added user tests (#1)

--- UserService/src/main/java/com/manish/user/dao/RoleDAO.java
+++ UserService/src/main/java/com/manish/user/dao/RoleDAO.java
@@ -3,10 +3,12 @@ package com.manish.user.dao;
 import com.manish.user.model.PermissionModel;
 import com.manish.user.model.RoleModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.Set;
 
+@Repository
 public interface RoleDAO extends JpaRepository<RoleModel, String> {
     Set<RoleModel> findByRoleNameIn(Set<String> roleNames);
     Optional<RoleModel> findByRoleName(String roleName);
