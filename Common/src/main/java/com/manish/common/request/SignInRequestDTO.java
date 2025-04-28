ce939484c321afffc23ba6000ea3b77218fcb649
manish <ms5978307@gmail.com>
2025-04-28 17:42:54 +0530

    bug fix

--- Common/src/main/java/com/manish/common/request/SignInRequestDTO.java
+++ Common/src/main/java/com/manish/common/request/SignInRequestDTO.java
@@ -1,12 +1,14 @@
 package com.manish.common.request;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class SignInRequestDTO {
     private String email;
     private String password;
--- UserService/src/main/java/com/manish/user/validate/AuthValidate.java
+++ UserService/src/main/java/com/manish/user/validate/AuthValidate.java
@@ -2,7 +2,6 @@ package com.manish.user.validate;
 
 import com.manish.common.request.SignInRequestDTO;
 import com.manish.common.util.StringUtil;
-import com.manish.user.dao.UserDAO;
 import com.manish.user.exception.ApplicationException;
 import com.manish.user.model.UserModel;
 import lombok.RequiredArgsConstructor;
@@ -14,25 +13,18 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class AuthValidate {
-    private final UserDAO userDAO;
-    private final PasswordEncoder passwordEncoder;
-
-    public UserModel validateSignInRequestDTO(SignInRequestDTO signInRequestDTO) {
-        Optional<UserModel> userModelOptional = userDAO.findByEmail(signInRequestDTO.getEmail());
-
-        if(!StringUtil.isEmpty(signInRequestDTO.getEmail())) {
+    public void validateSignInRequestDTO(SignInRequestDTO signInRequestDTO, PasswordEncoder passwordEncoder, Optional<UserModel> userModelOptional) {
+        if(StringUtil.isEmpty(signInRequestDTO.getEmail())) {
             throw new ApplicationException("Email cannot be empty");
         }
         if (userModelOptional.isEmpty()) {
             throw new ApplicationException("User not found with email: " + signInRequestDTO.getEmail());
         }
-        if(!StringUtil.isEmpty(signInRequestDTO.getPassword())) {
+        if(StringUtil.isEmpty(signInRequestDTO.getPassword())) {
             throw new ApplicationException("Password cannot be empty");
         }
         if (!passwordEncoder.matches(signInRequestDTO.getPassword(), userModelOptional.get().getHashPassword())) {
             throw new ApplicationException("Invalid password");
         }
-
-        return userModelOptional.get();
     }
 }
